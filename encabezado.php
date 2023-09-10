8a74463f268586c5ab7e0d260294b28e93b9b8d9
nicog2112 <gaunanicolas3@gmail.com>
2023-09-10 20:53:12 -0300

    Agrega archivos iniciales del proyecto

---
+++ encabezado.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema de Ventas</title>
+	
+	<link rel="stylesheet" href="/G2Unaf/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="/G2Unaf/css/bootstrap.css">
+	<link rel="stylesheet" href="/G2Unaf/css/estilo.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="/G2Unaf/Inicio.php">G2 Accesorios</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="/G2Unaf/Inicio.php">Inicio</a></li>
+					<li><a href="/G2Unaf/modulos/Productos/listar.php">Productos</a></li>
+					<li><a href="/G2Unaf/modulos/Ventas/ventas.php">Ventas</a></li>
+					<li><a href="/G2Unaf/modulos/Categoria/listar.php">Categorias</a></li>
+					<li><a href="/G2Unaf/modulos/Login/login.php">Cerrar Sesion</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
